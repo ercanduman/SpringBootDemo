46645ca00807973cb80f3b4e41cb8f2b2870eb64
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-10-01 12:31:07 +0300

    PostgreSqlPersonDaoImpl.kt object created and Changing only QUALIFIER_NAME is enough to switch DAO resource

---
+++ src/main/kotlin/ercanduman/springbootdemo/data/dao/PostgreSqlPersonDaoImpl.kt
@@ -0,0 +1,28 @@
+package ercanduman.springbootdemo.data.dao
+
+import ercanduman.springbootdemo.Constants
+import ercanduman.springbootdemo.data.entity.Person
+import org.springframework.stereotype.Repository
+
+@Repository(Constants.QUALIFIER_POSTGRESQL_DATA)
+class PostgreSqlPersonDaoImpl : PersonDao {
+    override fun personList(): List<Person> {
+        return listOf(Person(id = 33, name = "Data from postgres"))
+    }
+
+    override fun getPerson(id: Int): Person? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(person: Person): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(person: Person): Int {
+        TODO("Not yet implemented")
+    }
+}
